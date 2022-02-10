3986b95c9b17fc8473de662f46782f4c0c478da5
bryancol27 <bryan.2709cas@gmail.com>
2022-02-10 12:03:18 -0500

    Changes in the files names and folders

--- Flowers.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("scroll", function(){
-    var header = document.querySelector("nav");
-    header.classList.toggle("abajo", window.scrollY > 0)
-})
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Paradise-Flowers
+
+# In this Zone you can find a little try with my skills in css and my semantics in html
